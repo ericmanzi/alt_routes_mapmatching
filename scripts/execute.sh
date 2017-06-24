7fdf1e66f0d0a2fdc2cc8bc123d3a2d61ef230b9
ericmanzi <ericmanzi@github.com>
2017-06-24 19:29:36 -0400

    exec

--- scripts/execute.sh
+++ scripts/execute.sh
@@ -2,6 +2,22 @@ cd /home/freights/apps/alt_routes_mapmatching
 git pull
 START=$1
 END=$2
-echo "Execute map-matching for alternate routes in range $START to $END..."
+echo "Executing map-matching for alternate routes in range $START to $END..."
 #./scripts/route_choice_modeling/route_map_match_job.rb
 
+#@echo "Preparing $postgres.conf"
+#	@echo "Checking for committed temporary files..."
+#	@if git ls-files | grep -E 'mrtmp|mrinput' > /dev/null; then \
+#		echo "" ; \
+#		echo "OBS! You have committed some large temporary files:" ; \
+#		echo "" ; \
+#		git ls-files | grep -E 'mrtmp|mrinput' | sed 's/^/\t/' ; \
+#		echo "" ; \
+#		exit 1 ; \
+#	fi
+
+#[ df -h ] {
+	echo "-----------STOP!----------"
+	echo "[Error] Segmentation fault: Check Virtualization settings if running on VM."
+	echo "A Networking or Memory error was encountered when running this script"
+#}
