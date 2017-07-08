ad605d55cd212c9a9907569b28f39831eb8f5d7e
ermanzi <ermanzi@mit.edu>
2017-07-08 03:09:58 -0400

    .

---
+++ scripts/set_mm_ids.sh
@@ -0,0 +1,14 @@
+PROJECT_HOME=/home/freights/apps/alt_routes_mapmatching
+cd $PROJECT_HOME
+git pull
+
+# Kill server if running
+if [ -f tmp/pids/server.pid ]; then
+	echo "Killing dev server..."
+	kill -9 $(cat tmp/pids/server.pid)
+fi
+# Start server
+echo "Restarting dev server..."
+rails server -de development
+
+rails runner -e development app/jobs/set_mm_id.rb
