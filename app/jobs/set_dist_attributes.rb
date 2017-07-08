009f7c6bd0984e8b6188db7784ef97809260abf2
ermanzi <ermanzi@mit.edu>
2017-07-08 01:13:05 -0400

    change mms ID to numeric

--- app/jobs/set_dist_attributes.rb
+++ app/jobs/set_dist_attributes.rb
@@ -1,7 +1,9 @@
 # allow bigint for id
-set_id_query = "ALTER TABLE map_matched_segments ALTER COLUMN id TYPE bigint;"
+set_id_type = "ALTER TABLE map_matched_segments ALTER COLUMN id TYPE numeric;"
+set_id = "UPDATE map_matched_segments SET id=(((extract(epoch from created_at)+source_id+1)*(extract(epoch from created_at)+source_id))/2.0)+source_id;"
 set_pkey = "ALTER TABLE map_matched_segments ADD PRIMARY KEY (id);"
-ActiveRecord::Base.connection.execute set_id_query
+ActiveRecord::Base.connection.execute set_id_type
+ActiveRecord::Base.connection.execute set_id
 ActiveRecord::Base.connection.execute set_pkey
 
 # set id and polyline
